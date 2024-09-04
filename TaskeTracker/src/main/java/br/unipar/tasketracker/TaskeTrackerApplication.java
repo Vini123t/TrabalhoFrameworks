58c9a0fb99f45216668a05ddaca2ed6578edd19c
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-09-03 21:32:29 -0300

    back

---
+++ TaskeTracker/src/main/java/br/unipar/tasketracker/TaskeTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.tasketracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskeTrackerApplication.class, args);
+    }
+
+}
---
+++ TaskeTracker/src/test/java/br/unipar/tasketracker/TaskeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.tasketracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
